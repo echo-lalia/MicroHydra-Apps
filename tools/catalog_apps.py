fb6d5e6f0f8c0a31bffbd18960bff8b9c5c69351
echo-lalia <108598670+echo-lalia@users.noreply.github.com>
2024-08-19 14:56:45 -0700

    Create details.yml and README.md files

--- tools/catalog_apps.py
+++ tools/catalog_apps.py
@@ -23,9 +23,19 @@ def main():
     
     for app in app_sources:
         app.make_readme()
+    
+    # update main README file with data from app_sources
+    update_main_readme(app_sources)
 
+    # make a small catalog of apps for each device. 
+    # (Designed to be easily downloaded/read from the device)
+    make_device_catalogs()
 
-
+    # compile apps into .mpy files
+    compile_mpy_apps()
+    
+    # zip apps to output folder for easy download from MicroHydra
+    zip_apps()
 
 
 
@@ -50,10 +60,42 @@ class AppSource:
         except FileNotFoundError:
             print(f"WARNING: App {self.name} has no details.yml")
         
+        # clean device names by converting to lowercase
+        self.details['devices'] = [name.lower() for name in self.details['devices']]
+
+        # find app source url (for linking to app)
+        self.url = self._get_app_url()
+
         # load app name (name of app in MicroHydra, might be different than folder name)
         self.app_name = self._get_app_name()
 
-    
+        self.author_string = self._make_author_string()
+        self.license_string = self._make_license_string()
+
+
+    def _get_app_url(self):
+        url_base = "https://github.com/echo-lalia/MicroHydra-Apps/tree/main/app-source/"
+        return url_base + self.name
+
+
+
+    def _make_author_string(self):
+        # make linked author string
+        if self.details['author_link']:
+            return f"[{self.details['author']}]({self.details['author_link']})"
+        else:
+            return self.details['author']
+
+
+    def _make_license_string(self):
+        # make linked/formatted license string
+        if self.details['license']:
+            if self.details['license_link']:
+                return f"[{self.details['license']}]({self.details['license_link']})"
+            return self.details['license']
+        return "?"
+
+
     def _get_app_name(self):
         """Search for module folder or file in app folder"""
         for dir_entry in os.scandir(os.path.join(APP_SOURCE, self.name)):
@@ -77,22 +119,13 @@ class AppSource:
         Generate README.md file for this app
         """
 
-        # make linked author string
-        if self.details['author_link']:
-            author_string = f"[{self.details['author']}]({self.details['author_link']})"
-        else:
-            author_string = self.details['author']
-        
-        # do the same for license string
-        license_string = f"[{self.details['license']}]({self.details['license_link']})"
-
         # assemble this app's README.md file
         readme = f"""\
 <!---
 This file is generated from the "details.yml" file. (Any changes here will be overwritten)
 --->
 # {self.name}
-> Author: **{author_string}** | License: **{license_string}** | Version: **{self.details['app_version']}**  
+> Author: **{self.author_string}** | License: **{self.license_string}** | Version: **{self.details['app_version']}**  
 > App name: **{self.app_name.removesuffix(".py")}**
 <br/>
 
@@ -108,5 +141,77 @@ This file is generated from the "details.yml" file. (Any changes here will be ov
         # finally, write that file
         with open(os.path.join(APP_SOURCE, self.name, 'README.md'), 'w', encoding="utf-8") as readme_file:
             readme_file.write(readme)
+
+
+
+
+
+def update_main_readme(app_sources):
+    """
+    This function reads all the apps provided in app_sources, and creates a main README.md file
+    """
+    # collect a set of all device names referenced by apps:
+    all_devices = {device for app in app_sources for device in app.details['devices']}
+    
+    readme_text = """
+<!---
+This file is generated from automatically. (Any changes here will be overwritten)
+--->
+"""
+
+    with open("README-header.md", "r", encoding="utf-8") as header_text:
+        readme_text += header_text.read()
+    
+    readme_text += """
+
+# Apps by device:  
+
+"""
+    # add links for apps by device:
+    for device in all_devices:
+        readme_text += f"- [{device.title()}](#{device})\n"
+    
+    readme_text += """
+<br/><br/>
+
+"""
+
+    # add apps by device:
+    for device in all_devices:
+        readme_text += f"""
+## {device.title()}
+
+"""
+        for app in app_sources:
+            if device in app.details['devices']:
+                readme_text += f"""\
+### [{app.name}]({app.url})  
+> Author: **{app.author_string}** | License: **{app.license_string}** | Version: **{app.details['app_version']}**  
+> {app.details['short_description']}
+<br/>
+
+"""
+    
+    with open("README.md", 'w', encoding="utf-8") as readme_file:
+        readme_file.write(readme_text)
+
+
+
+def make_device_catalogs():
+    # update main README file with data from app_sources
+    pass
+    
+
+def compile_mpy_apps():
+    # compile apps into .mpy files
+    pass
+
+
+def zip_apps():
+    # zip apps to output folder for easy download from MicroHydra
+    pass
+
+
+
 
 main()
